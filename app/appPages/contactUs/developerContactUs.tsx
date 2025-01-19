17b25b774aae57bb64723b158e5e70f412cf5453
Jonathan Hill <jonathan.r.hill@outlook.com>
2025-01-19 19:59:12 +0000

    implemented persistant username to other pages: implemented bug/feature post to db.  corrected api url in the service for this

--- app/appPages/contactUs/developerContactUs.tsx
+++ app/appPages/contactUs/developerContactUs.tsx
@@ -1,87 +1,82 @@
-import React, { useEffect, useState }  from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Alert,
-} from "react-native";
-import { useRouter } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { useRouter, useLocalSearchParams } from "expo-router";
 
 import Navbar from "../../Navbar";
 
 export default function DevContactMain() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const reportBug = () => {
-        try{
-            console.log("Button Pressed");
-            router.push("./reportBug");
-        } catch(error) {
-            console.error("Navigation Error", error);
-            Alert.alert("Navigation Error", "Could not navigate to correct page");
-        }
+  const { username } = useLocalSearchParams();
+
+  const reportBug = () => {
+    try {
+      console.log("Report Bug Button Pressed");
+      router.push({ pathname: "./reportBug", params: { username } });
+    } catch (error) {
+      console.error("Navigation Error", error);
+      Alert.alert("Navigation Error", "Could not navigate to correct page");
     }
+  };
 
-    const requestFeature = () => {
-        try{
-            console.log("Button Pressed");
-            router.push("./requestFeature");
-        } catch(error) {
-            console.error("Navigation Error", error);
-            Alert.alert("Navigation Error", "Could not navigate to correct page");
-        }
+  const requestFeature = () => {
+    try {
+      console.log("Request Feature Button Pressed");
+      router.push({ pathname: "./requestFeature", params: { username } });
+    } catch (error) {
+      console.error("Navigation Error", error);
+      Alert.alert("Navigation Error", "Could not navigate to correct page");
     }
+  };
 
-    const discordServer = () => {
-        try{
-            console.log("Button Pressed");
-            router.push("./discordServer"); //note this may be an actual link to a discord server in the future
-        } catch(error) {
-            console.error("Navigation Error", error);
-            Alert.alert("Navigation Error", "Could not navigate to correct page");
-        }
+  const discordServer = () => {
+    try {
+      console.log("Button Pressed");
+      router.push("./discordServer"); //note this may be an actual link to a discord server in the future
+    } catch (error) {
+      console.error("Navigation Error", error);
+      Alert.alert("Navigation Error", "Could not navigate to correct page");
     }
+  };
 
-    const gitHub = () => {
-        try{
-            console.log("Button Pressed");
-            router.push("https://github.com/Jonathan-R-Hill/BabyHealthApp"); //note this may be an actual link to the github
-        } catch(error) {
-            console.error("Navigation Error", error);
-            Alert.alert("Navigation Error", "Could not navigate to correct page");
-        }
+  const gitHub = () => {
+    try {
+      console.log("Button Pressed");
+      router.push("https://github.com/Jonathan-R-Hill/BabyHealthApp"); //note this may be an actual link to the github
+    } catch (error) {
+      console.error("Navigation Error", error);
+      Alert.alert("Navigation Error", "Could not navigate to correct page");
     }
+  };
 
-    return (
-        <View style = {styles.container}>
-            <Text style = {styles.textTitle}>Developer Contact</Text>
-            
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textTitle}>Developer Contact</Text>
 
-            {/*Button Placement*/}
-            <View style = {styles.header}>
-                <TouchableOpacity style={styles.chartButton} onPress={reportBug}>
-                    <Text style={styles.chartButtonText}>Report A Bug</Text>
-                </TouchableOpacity>
-            </View>
-            <View style = {styles.header}>
-                <TouchableOpacity style={styles.chartButton} onPress={requestFeature}>
-                    <Text style={styles.chartButtonText}>Request A Feature</Text>
-                </TouchableOpacity>
-            </View>
-            <View style = {styles.header}>
-                <TouchableOpacity style={styles.chartButton} onPress={discordServer}>
-                    <Text style={styles.chartButtonText}>App Support Discord Server</Text>
-                </TouchableOpacity>
-            </View>
-            <View style = {styles.header}>
-                <TouchableOpacity style={styles.chartButton} onPress={gitHub}>
-                    <Text style={styles.chartButtonText}>GitHub</Text>
-                </TouchableOpacity>
-            </View>
+      {/*Button Placement*/}
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.chartButton} onPress={reportBug}>
+          <Text style={styles.chartButtonText}>Report A Bug</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.chartButton} onPress={requestFeature}>
+          <Text style={styles.chartButtonText}>Request A Feature</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.chartButton} onPress={discordServer}>
+          <Text style={styles.chartButtonText}>App Support Discord Server</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.chartButton} onPress={gitHub}>
+          <Text style={styles.chartButtonText}>GitHub</Text>
+        </TouchableOpacity>
+      </View>
 
-            {/* Bottom Navigation */}
-            {/* <View style={styles.bottomNav}>
+      {/* Bottom Navigation */}
+      {/* <View style={styles.bottomNav}>
                 <TouchableOpacity style={styles.navButton}>
                 <Text>Diary</Text>
                 </TouchableOpacity>
@@ -95,45 +90,45 @@ export default function DevContactMain() {
                 <Text>More Options</Text>
                 </TouchableOpacity>
             </View> */}
-            <Navbar />
-        </View>
-    );
+      <Navbar />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-    },
-    header: {
-      padding: 10,
-      flexDirection: "column",
-      justifyContent: "flex-start",
-      alignItems: "center",
-      flex: 2
-    },
-    chartButton: {
-      backgroundColor: "#3498db",
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 5,
-      height: "30%",
-      width: "80%",
-      maxHeight: 300,
-      maxWidth: 600,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    chartButtonText: {
-      color: "#fff",
-      fontWeight: "bold",
-      alignSelf: "center",
-    },
-    textTitle: {
-      color: "#000000",
-      fontSize: 40,
-      fontWeight: "bold",
-      flex: 1,
-      textAlign: "center",
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  header: {
+    padding: 10,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flex: 2,
+  },
+  chartButton: {
+    backgroundColor: "#3498db",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    height: "30%",
+    width: "80%",
+    maxHeight: 300,
+    maxWidth: 600,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  chartButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+  },
+  textTitle: {
+    color: "#000000",
+    fontSize: 40,
+    fontWeight: "bold",
+    flex: 1,
+    textAlign: "center",
+  },
 });
--- app/appPages/contactUs/requestFeature.tsx
+++ app/appPages/contactUs/requestFeature.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }  from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -7,53 +7,58 @@ import {
   Alert,
   TextInput,
 } from "react-native";
-import { useRouter } from "expo-router";
+import { useRouter, useLocalSearchParams } from "expo-router";
 
 import Navbar from "../../Navbar";
+import { submitFeature } from "../../../services/contactUsService";
 
 export default function requestFeatureMain() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const [requestText, setRequestText] = useState("");
+  const { username } = useLocalSearchParams();
 
-    const handleReportSend = () => {
-        try{
-            console.log("feature:",requestText);
+  const [requestText, setRequestText] = useState("");
 
-            router.push("./developerContactUs");
-        } catch (error) {
-            console.error("Error Requesting Feature", error);
-            Alert.alert("Error Requesting Feature, your report has not been sent");
-        }
-    }
+  const handleReportSend = async () => {
+    submitFeature(String(username), requestText)
+      .then(() => {
+        console.log("Diary entry created successfully!");
+      })
+      .then(() => {
+        router.push({ pathname: "./developerContactUs", params: { username } });
+      })
+      .catch((error) => {
+        console.error("Error creating diary entry:", error);
+      });
+  };
 
-    return (
-        <View style = {styles.container}>
-            {/*Title Placement*/}
-            <View style = {styles.header}>
-                <Text style = {styles.textTitle}>Request A Feature</Text>
-            </View>
+  return (
+    <View style={styles.container}>
+      {/*Title Placement*/}
+      <View style={styles.header}>
+        <Text style={styles.textTitle}>Request A Feature</Text>
+      </View>
 
-            {/*Text Box Placement*/}
-            <View style = {styles.inputBoxContainer}>
-                <TextInput
-                style={styles.inputBox}
-                placeholder="Request a feature here"
-                value={requestText}
-                onChangeText={setRequestText}
-                autoCapitalize="none"
-                autoCorrect={true}
-                keyboardType="default"
-                multiline={true}
-                placeholderTextColor={"#84868a"}
-                />
-                <TouchableOpacity style={styles.chartButton} onPress={handleReportSend}>
-                    <Text style={styles.chartButtonText}>Send Report</Text>
-                </TouchableOpacity>
-            </View>
+      {/*Text Box Placement*/}
+      <View style={styles.inputBoxContainer}>
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Request a feature here"
+          value={requestText}
+          onChangeText={setRequestText}
+          autoCapitalize="none"
+          autoCorrect={true}
+          keyboardType="default"
+          multiline={true}
+          placeholderTextColor={"#84868a"}
+        />
+        <TouchableOpacity style={styles.chartButton} onPress={handleReportSend}>
+          <Text style={styles.chartButtonText}>Send Report</Text>
+        </TouchableOpacity>
+      </View>
 
-            {/* Bottom Navigation */}
-            {/* <View style={styles.bottomNav}>
+      {/* Bottom Navigation */}
+      {/* <View style={styles.bottomNav}>
                 <TouchableOpacity style={styles.navButton}>
                 <Text>Diary</Text>
                 </TouchableOpacity>
@@ -67,53 +72,53 @@ export default function requestFeatureMain() {
                 <Text>More Options</Text>
                 </TouchableOpacity>
             </View> */}
-            <Navbar />
-        </View>
-    )
+      <Navbar />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-    },
-    header: {
-      padding: 10,
-      flexDirection: "column",
-      justifyContent: "flex-start",
-      alignItems: "center",
-    },
-    chartButton: {
-      backgroundColor: "#3498db",
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 5,
-      marginLeft: "auto",
-    },
-    chartButtonText: {
-      color: "#fff",
-      fontWeight: "bold",
-    },
-    textTitle: {
-        color: "#000000",
-        fontSize: 40,
-        fontWeight: "bold",
-        textAlign: "center",
-    },
-    inputBox: {
-        height: "80%",
-        width: "100%",
-        borderColor: "blue",
-        borderWidth: 1,
-        marginBottom: 16,
-        borderRadius: 4,
-        paddingHorizontal: 8,
-        backgroundColor: "#e8e6e1",
-        textAlign: "center",
-    },
-    inputBoxContainer: {
-        height: "80%",
-        width: "90%",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  header: {
+    padding: 10,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  chartButton: {
+    backgroundColor: "#3498db",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginLeft: "auto",
+  },
+  chartButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  textTitle: {
+    color: "#000000",
+    fontSize: 40,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  inputBox: {
+    height: "80%",
+    width: "100%",
+    borderColor: "blue",
+    borderWidth: 1,
+    marginBottom: 16,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    backgroundColor: "#e8e6e1",
+    textAlign: "center",
+  },
+  inputBoxContainer: {
+    height: "80%",
+    width: "90%",
+  },
 });
