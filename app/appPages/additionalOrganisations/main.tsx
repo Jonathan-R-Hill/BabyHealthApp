4c79dbe2393ce45b5e8556080f0a2259ae8d6f0e
Nathan-H66 <fusionflares3@gmail.com>
2025-03-21 12:32:13 +0000

    Additional org page has been populated with 2 organisations, more needed. Elements in the list have also been styled for presentation but may need revisting.

--- app/appPages/additionalOrganisations/main.tsx
+++ app/appPages/additionalOrganisations/main.tsx
@@ -29,14 +29,23 @@ export default function additionalOrgs() {
             data={orgData}
             keyExtractor={(item, index) => index.toString()}
             renderItem={({ item }) => (
-            <View style={{ marginBottom: 10 }}>
-                <Text>{item.orgName}</Text>
-                <Text>{item.orgDesc}</Text>
-                <TouchableOpacity onPress={() => handlePress(item.orgLink)}>
-                <Text style={{ color: 'blue' }}>{item.orgLink}</Text>
-                </TouchableOpacity>
-                <Text>{item.orgEmail}</Text>
-                <Text>{item.orgPhone}</Text>
+              <View style={styles.listItem}>
+              <Text style={styles.orgName}>{item.orgName}</Text>
+              <Text style={styles.orgDesc} numberOfLines={3}>{item.orgDesc}</Text>
+              <View style={styles.row}>
+              <Text style={styles.label}>Website: </Text>
+              <TouchableOpacity onPress={() => handlePress(item.orgLink)}>
+                <Text style={styles.linkText}>{item.orgLink}</Text>
+              </TouchableOpacity>
+              </View>
+              <View style={styles.row}>
+              <Text style={styles.label}>Email: </Text>
+              <Text style={styles.contactInfo}>{item.orgEmail}</Text>
+              </View>
+              <View style={styles.row}>
+              <Text style={styles.label}>Phone Number: </Text>
+              <Text style={styles.contactInfo}>{item.orgPhone}</Text>
+              </View>
             </View>
             )}
         />
@@ -60,6 +69,47 @@ const styles = StyleSheet.create({
       color: "#000000",
       marginBottom: 20,
     },
-    
+    listItem: {
+      borderRadius: 8, 
+      padding: 15, 
+      marginBottom: 10, 
+      width: "90%", 
+    },
+    orgName: {
+      fontSize: 24,
+      fontWeight: "bold",
+      marginBottom: 5,
+      alignSelf: "center"
+    },
+    orgDesc: {
+      fontSize: 18,
+      marginBottom: 10,
+      alignSelf: "center"
+    },
+    linkText: {
+      color: "blue",
+      fontSize: 18,
+      textDecorationLine: "underline",
+      marginBottom: 3,
+      alignSelf: "center"
+    },
+    contactInfo: {
+      fontSize: 18,
+      marginBottom: 3,
+      alignSelf: "center"
+    },
+    label: {
+      fontSize: 18,
+      fontWeight: "bold",
+      marginBottom: 3,
+      alignSelf: "center"
+    },
+    row: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      marginBottom: 10,
+    },
+  
   });
   
--- app/appPages/additionalOrganisations/orgData.ts
+++ app/appPages/additionalOrganisations/orgData.ts
@@ -1,5 +1,8 @@
 export const orgData = [
-    {id: "1", orgName: "test", orgDesc: "test dec", 
-        orgLink: "https://www.google.com", orgEmail: "testEmail", 
-        orgPhone: "0123456789"}
+    {id: "1", orgName: "Bliss", orgDesc: "Bliss is an organisation that aims to help parents with premature or sick babies, providing support to help their child have the best chance at survival and quality of life.", 
+        orgLink: "https://www.bliss.org.uk", orgEmail: "hello@bliss.org.uk", 
+        orgPhone: "N/A"},
+    {id: "2", orgName: "Rainbow Trust", orgDesc: "Rainbow Trust provides emotional and practial support to families with a seriously ill child.", 
+        orgLink: "https://www.rainbowtrust.org.uk", orgEmail: "enquiries@rainbowtrust.org.uk", 
+        orgPhone: "01372 363438"}
 ]
