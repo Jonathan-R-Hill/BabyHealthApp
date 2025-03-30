933df9e0b66b36ddafee8d94331425fe45a2379c
DrEpico <76069708+DrEpico@users.noreply.github.com>
2025-03-30 21:47:12 +0100

    Add corner style option to ReusableButton (rounded or sharp).

--- components/ReusableButton.tsx
+++ components/ReusableButton.tsx
@@ -1,8 +1,9 @@
-import { Pressable, PressableProps, Text } from "react-native";
+import { Pressable, PressableProps, Text, StyleSheet } from "react-native";
 
 // I'm creating a type for the props my ReusableButton will accept.
 type ReusableButtonProps = PressableProps & {//PressableProbs AND... 
     title?: string; //A text label for the button (e.g. "Login")
+    edge?: "round" | "edgy" ;
     children?: React.ReactNode; //The child element inside the ReusableButton component.
 };
 
@@ -18,22 +19,32 @@ type ReusableButtonProps = PressableProps & {//PressableProbs AND...
  * Quick use: just pass title="Login".
  * Advanced use: customise everything using children.
  */
-export function ReusableButton({ title, children, ...otherProps}: ReusableButtonProps){
+export function ReusableButton({ title, edge, children, ...otherProps}: ReusableButtonProps){
 //In params, "Desctructure" the probs to apply them directly to <Pressable>
+    const sizeStyles = {
+        round: styles.roundEdge,
+        edgy: styles.edgyEdge,
+    }
 
-    return <Pressable style={{
-        backgroundColor: "#65558F", 
-        borderRadius: 20, 
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        minHeight: 40,
-        // height: "30%", /* Disabled to adjust the height of the element to the size of the child element rather than the parent element. */
-        width: "100%", //Leave the default width at 100% and individually adjust it for specific buttons if needed.
-        maxHeight: 300,
-        alignItems: "center", //This keeps the child element centered.
-        justifyContent: "center", //Think of this as alignItem's emotional support property.
-        //maxWidth: 600
-    }} {...otherProps}> 
+    return <Pressable 
+        style={[
+        {
+            backgroundColor: "#65558F", 
+            borderRadius: 20, 
+            paddingVertical: 10,
+            paddingHorizontal: 20,
+            minHeight: 40,
+            // height: "30%", /* Disabled to adjust the height of the element to the size of the child element rather than the parent element. */
+            width: "100%", //Leave the default width at 100% and individually adjust it for specific buttons if needed.
+            maxHeight: 300,
+            alignItems: "center", //This keeps the child element centered.
+            justifyContent: "center", //Think of this as alignItem's emotional support property.
+            //maxWidth: 600
+        },
+        sizeStyles[edge ?? "round"]
+    ]} 
+    {...otherProps}
+    > 
         {title ? ( //Conditional statement: if title exists, show it in a white <Text> element.
             <Text style={{ color: "white", fontWeight: "600" }}>{title}</Text>
         ) : ( 
@@ -41,3 +52,12 @@ export function ReusableButton({ title, children, ...otherProps}: ReusableButton
         )}
     </Pressable>
 }
+
+const styles = StyleSheet.create({
+    roundEdge: {
+        marginBottom: 25,
+    },
+    edgyEdge: {
+        borderRadius: 8,
+    }
+});
