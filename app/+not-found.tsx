625de82e066f1d3ff367977621c91a3336b679f2
DrEpico <76069708+DrEpico@users.noreply.github.com>
2025-04-21 17:01:50 +0100

    Add header and footer to 404 page.

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -4,15 +4,19 @@ import { StyleSheet } from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import Header from "../app/Header";
+import Navbar from "../app/Navbar";
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
       <ThemedView style={styles.container}>
+        <Header title="404"></Header>
         <ThemedText type="title">
           This screen doesn't exist. Please go back
         </ThemedText>
+        <Navbar></Navbar>
       </ThemedView>
     </>
   );
